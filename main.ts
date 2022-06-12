052b55584f5371a351a95b6b8d9ef035dc53fc69
Vik Borges <v1k@protonmail.com>
2022-06-12 23:31:35 +0100

    rough flow of the app

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-console.log("hello");
+
+const nginx_file = Deno.args[0] || prompt("Enter the path to the nginx file to be processed:", "nginx.conf")
+if(nginx_file == null) console.log('we need a file yo...'), Deno.exit(1)
+
+const nginx_symlink = prompt("What's the name for the symbolic link file:");
+if(nginx_symlink == null) console.log('we need to know where to put the file...'), Deno.exit(1)
+
+await Deno.symlink(nginx_file, nginx_symlink)
+
+// profit?!
